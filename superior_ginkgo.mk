1cf178a8867610436af3fcfaff448aadbdd676d8
madmax7896 <thegreatsagar123@gmail.com>
2020-08-22 03:25:48 +0530

    Go official

--- superior_ginkgo.mk
+++ superior_ginkgo.mk
@@ -39,6 +39,9 @@ PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+#OFFICIAL
+export SUPERIOR_OFFICIAL=true
+
 # Fingerprint
 BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
 
